bcb715c2e8ae87e8c130d31d4dc6bcec778846f4
kuiyuanc <kono12041225@nycu.edu.tw>
2023-05-24 09:21:15 +0800

    quick try of LSTM agent

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from database import database
 from crawler import crawler
 from rater import rater
 from ui import ui
+from sklearn.model_selection import train_test_split
+
 
 class anime_recommender:
     """docstring for animeRecommender"""
@@ -24,7 +26,43 @@ class anime_recommender:
         pass
 
 
+def quick_try():
+    import os
+    import csv
+
+    # load reviews from reviews.csv
+    reviews = []
+    data_files = os.listdir('./data')
+    for file in data_files:
+        csvfile = open('./data/' + file, newline='', encoding="utf-8")
+        reader = csv.reader(csvfile)
+        for name, text, rating in reader:
+            if name == 'Anime':
+                continue
+            rate = int(rating.replace('Reviewer’s Rating:', '').replace(' ', '').replace('\n', ''))
+            reviews.append([name, text, rate])
+        csvfile.close()
+    print('loaded successfully')
+
+    # turn reviews into valid input
+    names, texts, ratings = zip(*reviews)
+    ratings = [rating / 10 for rating in ratings]
+    Xtrain, Xtest, ytrain, ytest = train_test_split(texts, ratings, train_size=.9, random_state=42)
+
+    # train & validate rater
+    r = rater()
+    print('validating:')
+    r.train(Xtrain, ytrain)
+
+    # test rater
+    print('testing:')
+    r.test(Xtest, ytest)
+
+    print('rating of an positive AI article:')
+    scores = r.rate('There are many positive aspects to AI! Here are just a few: 1. Improved efficiency: AI has the ability to process and analyze vast amounts of data quickly and accurately, leading to improved efficiency and productivity in various industries. 2. Personalization: AI algorithms can learn from user behavior and preferences, allowing for personalized recommendations and experiences. 3. Medical advances: AI has the potential to revolutionize healthcare by assisting doctors with diagnoses, predicting disease outbreaks, and developing new treatments. 4. Enhanced safety: AI can be used to monitor and detect potential safety hazards in real-time, such as detecting fraud or identifying potential security threats. 5. Accessibility: AI-powered technologies can provide accessibility features for people with disabilities, such as text-to-speech and speech recognition software. Overall, AI has the potential to positively impact many aspects of our lives, from healthcare and education to transportation and entertainment.')
+    for i in range(len(r.agents.keys())):
+        print(f'{r.agents.keys()[i]}: {scores[i]}')
+
+
 if __name__ == '__main__':
-    ar = anime_recommender()
-    ar.train()
-    ar.run()
+    quick_try()
